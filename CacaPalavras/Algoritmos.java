3f378764df6c47d3aef765abbce7caa1e6c3ee98
bedi1982 <bedi.com@gmail.com>
2012-03-12 11:21:10 -0300

    Updated code

--- CacaPalavras/Algoritmos.java
+++ CacaPalavras/Algoritmos.java
@@ -16,8 +16,13 @@ public class Algoritmos {
 	}
 
 	public static void palavras(){
+		char matriz[][] = Algoritmos.preencheMatriz();
 		char p1[] = {'I','t','a','l','i','a'};
-		Algoritmos.inserePalavras(p1, 1);
+		char p2[] = {'B','r','a','s','i','l'};
+		char p3[] = {'H','o','l','a','n','d','a'};
+		Algoritmos.inserePalavras(p1, 1, matriz);
+		Algoritmos.inserePalavras(p2, 2, matriz);
+		Algoritmos.inserePalavras(p3, 2, matriz);
 	}
 	
 	public static int[] ValidPositionGenerator(char palavra[], char matriz[][]){
@@ -29,32 +34,11 @@ public class Algoritmos {
 	int alignment = 1; // <- Teste
 	
 	if(alignment == 1){ //If he wants a 1-horizontal alignment
-	while(cont != palavra.length){
-		
-		Random randomGenerator = new Random();
-		j = randomGenerator.nextInt(20 - palavra.length);
-		i = randomGenerator.nextInt(10);
-		
-		validPosition[0] = i;
-		validPosition[1] = j;
-
-			for (int k = 0; k < palavra.length; k++){
-				if((matriz[i][j++]) == '*'){
-					cont++;
-				}else{
-					cont = 0;
-				}
-			}
-		}
-	return validPosition;
-	
-	}else{ //If he wants a 2-vertical alignment
 		while(cont != palavra.length){
-			
 			Random randomGenerator = new Random();
 			j = randomGenerator.nextInt(20 - palavra.length);
 			i = randomGenerator.nextInt(10);
-			
+		
 			validPosition[0] = i;
 			validPosition[1] = j;
 
@@ -65,15 +49,31 @@ public class Algoritmos {
 						cont = 0;
 					}
 				}
-			}
+		}
 		return validPosition;
 		
-	}
-	}
+	}else{ //If he wants a 2-vertical alignment
+		while(cont != palavra.length){
+			Random randomGenerator = new Random();
+			j = randomGenerator.nextInt(20 - palavra.length);
+			i = randomGenerator.nextInt(10);
+			validPosition[0] = i;
+			validPosition[1] = j;
 
-	public static int inserePalavras(char palavra[], int totalPalavras){
+				for (int k = 0; k < palavra.length; k++){
+					if((matriz[i++][j]) == '*'){
+						cont++;
+					}else{
+						cont = 0;
+					}
+				}
+			}
+			return validPosition;
+		}
+}
+
+	public static int inserePalavras(char palavra[], int totalPalavras, char [][]matriz){
 		
-		char matriz[][] = Algoritmos.preencheMatriz();
 		int validPosition[] = ValidPositionGenerator(palavra, matriz);
 		
 		
@@ -88,6 +88,9 @@ public class Algoritmos {
 					}
 			
 		Algoritmos.mostraMatriz(matriz);
+		System.out.println();
+		System.out.println();
+		System.out.println();
 		return 0;	
 	}
 
