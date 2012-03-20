fa3061cf967d9070cf9adf1f0fd048015a05284d
Sergio Rafael Lemke <bedi.com@gmail.com>
2012-03-20 15:25:00 -0300

    Final pull, i hope

--- CacaPalavras/Algoritmos.java
+++ CacaPalavras/Algoritmos.java
@@ -12,37 +12,47 @@ public class Algoritmos {
 				matriz[i][j] = '*';
 			}
 		}
-
 		return matriz;
 	}
 
 	//--------------------------------------------------------------------------------------------
-	public static void recebePalavras(){
+	public static char[][] recebePalavras(){
 		//1 = Horizontal //2 = Vertical : !!10 letras Max!!
-
 		char matriz[][] = Algoritmos.preencheMatriz(); //Recebo uma matriz cheia de *
-		//Um monte de testes
 		char p1[] = {'L','u','x','e','m','b','u','r','g','o'};
 		char p2[] = {'P','a','q','u','i','s','t','a','o'};
 		char p3[] = {'I','t','a','l','i','a'};
 		char p4[] = {'B','r','a','s','i','l'};
 		char p5[] = {'H','o','l','a','n','d','a'};
-		char p6[] = {'G','r','e','c','i','a'};
-		char p7[] = {'I','t','a','l','i','a'};
-		char p8[] = {'I','t','a','l','i','a'};
-		/*char p9[] = {'I','t','a','l','i','a'};
-		char p10[] = {'I','t','a','l','i','a'};
-		char p1[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
-		char p2[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
-		char p3[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
-		char p4[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
-		char p5[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
-		char p6[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
-		char p7[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
+		char p6[] = {'J','a','p','a','o'};
+		char p7[] = {'I','n','g','l','a','t','e','r','a'};
+		char p8[] = {'S','u','r','i','n','a','m','e'};
+
+		//testes de estouro e correções de matriz//
+		//char p9[] = {'I','t','a','l','i','a'};
+		//char p10[] = {'I','t','a','l','i','a'};
+		/*char p1[] = {'L','u','x','e','m','b','u','r','g','o','L','u','x','e','m','b','u','r','g','o'};
+		char p2[] = {'L','u','x','e','m','b','u','r','g','o','L','u','x','e','m','b','u','r','g','o'};
+		char p3[] = {'L','u','x','e','m','b','u','r','g','o','L','u','x','e','m','b','u','r','g','o'};
+		char p4[] = {'L','u','x','e','m','b','u','r','g','o','L','u','x','e','m','b','u','r','g','o'};
+		char p5[] = {'L','u','x','e','m','b','u','r','g','o','L','u','x','e','m','b','u','r','g','o'};
+		char p6[] = {'L','u','x','e','m','b','u','r','g','o','L','u','x','e','m','b','u','r','g','o'};
+		char p7[] = {'L','u','x','e','m','b','u','r','g','o','L','u','x','e','m','b','u','r','g','o'};
 		char p8[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
 		char p9[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
 		char p10[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
-		char p10[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0','a'};
+		char p11[] = {'L','u','x','e','m','b','u','r','g','0','L','u','x','e','m','b','u','r','g','0'};
+		char p1[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p1[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p2[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p3[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p4[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p5[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p6[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p7[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p8[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p9[] = {'L','u','x','e','m','b','u','r','g','0'};
+		char p10[] = {'L','u','x','e','m','b','u','r','g','0'};
 		char p11[] = {'L','u','x','e','m','b','u','r','g','0'};
 		char p12[] = {'L','u','x','e','m','b','u','r','g','0'};
 		char p13[] = {'L','u','x','e','m','b','u','r','g','0'};
@@ -64,34 +74,75 @@ public class Algoritmos {
 		Algoritmos.inserePalavras(p6, matriz);
 		Algoritmos.inserePalavras(p7, matriz);
 		Algoritmos.inserePalavras(p8, matriz);
-		/*Algoritmos.inserePalavras(p9, 9, matriz);
-		Algoritmos.inserePalavras(p10, 10, matriz);
-		Algoritmos.inserePalavras(p11, 11, matriz);
-		Algoritmos.inserePalavras(p12, 12, matriz);
-		Algoritmos.inserePalavras(p13, 13, matriz);
-		Algoritmos.inserePalavras(p14, 14, matriz);
-		Algoritmos.inserePalavras(p15, 15, matriz);
-		Algoritmos.inserePalavras(p16, 16, matriz);
-		Algoritmos.inserePalavras(p17, 17, matriz);
-		Algoritmos.inserePalavras(p18, 18, matriz);
-		Algoritmos.inserePalavras(p19, 19, matriz);
-		Algoritmos.inserePalavras(p20, 20, matriz);
-		Algoritmos.inserePalavras(p21, 21, matriz);*/
+		/*Algoritmos.inserePalavras(p9,  matriz);
+		Algoritmos.inserePalavras(p10, matriz);
+		Algoritmos.inserePalavras(p11, matriz);
+		Algoritmos.inserePalavras(p12, matriz);
+		Algoritmos.inserePalavras(p13, matriz);
+		Algoritmos.inserePalavras(p14, matriz);
+		Algoritmos.inserePalavras(p15, matriz);
+		Algoritmos.inserePalavras(p16, matriz);
+		Algoritmos.inserePalavras(p17, matriz);
+		Algoritmos.inserePalavras(p18, matriz);
+		Algoritmos.inserePalavras(p19, matriz);
+		Algoritmos.inserePalavras(p20, matriz);
+		Algoritmos.inserePalavras(p21, matriz);*/
+		return matriz;
+	}
+
+	//--------------------------------------------------------------------------------------------
+	public static boolean verificadorDeAcerto(int orientacao, int linha, int coluna, int comprimento, char matriz[][]){
+
+		int i;
+		int cont = 0;
+		int acertos = 0;
+
+		if(orientacao == 1){ //1 para horizontal
+			for(i = coluna; i < comprimento + coluna; i++){
+				if(matriz[linha][i] != '*'){
+					System.out.println("Letra acertada :" + matriz[linha][i]); //teste
+					matriz[linha][i] = 'X'; //teste
+					cont++;
+				}
+
+				if(cont == comprimento){
+					Algoritmos.mostraMatriz(matriz);
+					return true;
+					//System.out.println("Ganhou");//teste
+				}
+			}
+		}else{ //Vertical
+			for(i = linha; i < comprimento + linha; i++){
+				if(matriz[i][coluna] != '*'){
+					System.out.println("Letra acertada: " + matriz[i][coluna]); //teste
+					matriz[i][coluna] = 'X'; //teste
+					cont++;
+				}
+
+				if(cont == comprimento){
+					Algoritmos.mostraMatriz(matriz);
+					return true;
+				}
+			}
+		}
 		Algoritmos.mostraMatriz(matriz);
+		return false;
 	}
 
 	//--------------------------------------------------------------------------------------------	
-	public static char[] inversorDePalavras(char []palavra){ //Não funcionando direito
+	public static char[] inversorDePalavras(char palavra[]){
 
 		int len = palavra.length;
 		char[] temporario = new char[len];  
-		char[] resultado = new char[len];  
+		char[] resultado = new char[len];
+		int i;
+		int j; 
 
-		for (int i = 0; i < len; i++) {  
+		for (i = 0; i < len; i++) {  
 			temporario[i] = palavra[i];
 		}  
 
-		for (int j = 0; j < len; j++) {  
+		for (j = 0; j < len; j++) {  
 			resultado[j] = temporario[len - 1 - j];  
 		}  
 		return resultado;
@@ -100,27 +151,24 @@ public class Algoritmos {
 	//--------------------------------------------------------------------------------------------	
 	public static int []geradorDePosicaoValida(char palavra[], char matriz[][]){
 
-		int i = 0;
-		int j = 0;
+		int i, j, k;
 		int cont = 0;
 		//1 = Horizontal //2 = Vertical
 		int validPosition[] = {0, 0, 0};
 
 		//Horizontal ou vertical:
 		validPosition[2] = Principal.orientacao();
+		Random randomGenerator = new Random();
 
 		if(validPosition[2] == 1){ //Horizontal
-			Random randomGenerator = new Random();
-			
 			while(cont != palavra.length){
-
 				i = randomGenerator.nextInt(10); //Alguma das 10 linhas
-				j = randomGenerator.nextInt(20 - palavra.length); //Comprimento da linha menos tam da palavra
+				j = randomGenerator.nextInt(21 - palavra.length); //Comprimento da linha menos tam da palavra
 
 				validPosition[0] = i;
 				validPosition[1] = j;
 
-				for (int k = 0; k < palavra.length; k++){
+				for (k = 0; k < palavra.length; k++){
 					if((matriz[i][j++]) == '*'){
 						cont++;
 					}else{
@@ -129,19 +177,20 @@ public class Algoritmos {
 				}
 			}
 			return validPosition;
-		
+
 		}else{ //Vertical
-			Random randomGenerator = new Random();
-			
 			while(cont != palavra.length){
-				i = randomGenerator.nextInt(11 - palavra.length); //Altura ps. Tive que colocar palavra +1 
-				//porque senão o random dava pau ao ter que gerar um random de 0 (10 - tamanho da palavra). Mesmo caso acima.
+				if(palavra.length < 10 ){
+					i = randomGenerator.nextInt(11 - palavra.length);
+				}else{
+					i = 0;
+				}
 				j = randomGenerator.nextInt(20); //Alguma coluna até 20
 
 				validPosition[0] = i;
 				validPosition[1] = j;
 
-				for (int k = 0; k < palavra.length; k++){
+				for (k = 0; k < palavra.length; k++){
 					if((matriz[i++][j]) == '*'){
 						cont++;
 					}else{
@@ -154,11 +203,12 @@ public class Algoritmos {
 	}
 
 	//--------------------------------------------------------------------------------------------
-	public static char inserePalavras(char palavra[], char [][]matriz){
+	public static void inserePalavras(char palavra[], char [][]matriz){
 
 		int validPosition[] = Algoritmos.geradorDePosicaoValida(palavra, matriz); //Receberei um i, j e orientação válidos
 		int i = validPosition[0];
 		int j = validPosition[1];
+		int k;
 		//1 = Horizontal //2 = Vertical
 		int orientacao =  validPosition[2];
 
@@ -169,32 +219,24 @@ public class Algoritmos {
 		///Fim do bloco de inversao///
 
 		if(orientacao == 1){
-			for(int k = 0; k < palavra.length; k++){
+			for(k = 0; k < palavra.length; k++){
 				matriz[i][j++] = palavra[k];
-				if (k == palavra.length){
-					//break;
-					return 0;
-				}
 			}
 		}else{
-				for(int k = 0; k < palavra.length; k++){
-					matriz[i++][j] = palavra[k];
-					if (k == palavra.length){
-						//break;
-						return 0;
-					}
-				}
+			for(k = 0; k < palavra.length; k++){
+				matriz[i++][j] = palavra[k];
+			}
 		}
-		return 0;
 	}
 
 	//--------------------------------------------------------------------------------------------
-	//Só imprime a matriz que receber
+	//Só imprime a matriz que receber -- Falta adicionar a parte de enxer de letras random aonde tem *//
 	public static void mostraMatriz(char matriz[][]){
+		int i,j;
 		System.out.println("Tabuleiro preenchido, ainda sem caracteres aleatórios:");
 		System.out.println();
-		for(int i=0; i<10; i++){
-			for(int j=0; j<20; j++){
+		for(i = 0; i < 10; i++){
+			for(j = 0; j < 20; j++){
 				System.out.print(matriz[i][j]);
 			}
 			System.out.println();
--- CacaPalavras/Principal.java
+++ CacaPalavras/Principal.java
@@ -1,14 +1,27 @@
 import java.util.Random;
 
 public class Principal extends Console {
-	
-	public static void main(String[] args) {
-		char lala[] = {'T','e','s','t','e'};
-		System.out.println("teste tem: " + lala.length + " letras");
-		Algoritmos.recebePalavras();
-	}
 
-	//--------------------------------------------------------------------------------------------
+	public static void main(String[] args) {
+		
+		int acertos = 0;
+		char matriz[][] = new char [10][20];
+		matriz = Algoritmos.recebePalavras();
+		Algoritmos.mostraMatriz(matriz);
+
+		do{
+			int orientacao = Console.readInt("Qual a orietação da palavra? 1-Horizontal 2-Vertical");
+			int linha = Console.readInt("Em qual linha está a palavra?");
+			int coluna = Console.readInt("Em qual coluna está a palavra?");
+			int comprimento = Console.readInt("Quantas letras tem a palavra?");
+			if (Algoritmos.verificadorDeAcerto(orientacao, linha, coluna, comprimento, matriz)){
+				acertos++;
+			}
+		}while(acertos != 2);
+		
+		System.out.println("Fim");
+	}
+	//---------------------------------------------------------------------------------------1-----
 	public static int orientacao(){
 		Random randomGenerator = new Random();
 		int tmp = randomGenerator.nextInt(2);
